b7ef569fdcb768cc363d0d9d2f2666307f57fc19
Andrew Mason <mason@case.edu>
2015-08-19 08:23:52 -0400

    add handling for enumerable methods. closes #1

--- lib/mccabe/analyzer.rb
+++ lib/mccabe/analyzer.rb
@@ -3,6 +3,7 @@ require 'mccabe/parser'
 module McCabe
   class Analyzer
     BRANCH_TYPES = [:if, :while, :until, :for, :when, :and, :or]
+    ENUMERABLE_METHODS = Enumerable.instance_methods + [:each]
 
     def self.analyze(ast)
       results = {}
@@ -16,7 +17,21 @@ module McCabe
       nodes, complexity = [ast], 0
       until nodes.empty?
         node = nodes.shift
-        complexity += 1 if BRANCH_TYPES.include?(node.type)
+        if BRANCH_TYPES.include?(node.type)
+          complexity += 1
+        elsif node.type == :block
+          sent_method = node.children[0]
+          if sent_method.children.length == 2 &&
+              ENUMERABLE_METHODS.include?(sent_method.children[1])
+            complexity += 1
+          end
+        elsif node.type == :send
+          if node.children.length >= 3 &&
+              node.children[2].type == :block_pass &&
+              ENUMERABLE_METHODS.include?(node.children[1])
+            complexity += 1
+          end
+        end
         nodes += node.children.select { |child| child.is_a? ::Parser::AST::Node }
       end
       complexity
