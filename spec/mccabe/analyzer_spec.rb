5c5eced1ca22d70e6b60ce704e312f0bccf5edaf
Andrew Mason <mason@case.edu>
2015-08-27 19:20:32 -0400

    spec cleanup

--- spec/mccabe/analyzer_spec.rb
+++ spec/mccabe/analyzer_spec.rb
@@ -5,62 +5,52 @@ require 'mccabe/parser'
 
 RSpec.describe McCabe::Analyzer do
   describe '::complexity' do
-    context 'when the AST is straight-line code' do
-      it 'returns 0' do
-        code = <<-CODE
-        a = 1
-        b = 1 - a
-        a * b
-        CODE
-        ast = McCabe::Parser.parse code
-        expect(McCabe::Analyzer.complexity ast).to be 0
-      end
+    it 'does not count straight-line code' do
+      code = <<-CODE
+      a = 1
+      b = 1 - a
+      a * b
+      CODE
+      ast = McCabe::Parser.parse code
+      expect(McCabe::Analyzer.complexity ast).to be 0
     end
 
-    context 'when the AST contains simple branching' do
-      it 'returns the number of brancing points' do
-        code = <<-CODE
-        if foo
-          bar
-        elsif baz
-          qux
-        end
-        CODE
-        ast = McCabe::Parser.parse code
-        expect(McCabe::Analyzer.complexity ast).to be 2
+    it 'counts the number of branching points' do
+      code = <<-CODE
+      if foo
+        bar
+      elsif baz
+        qux
       end
+      CODE
+      ast = McCabe::Parser.parse code
+      expect(McCabe::Analyzer.complexity ast).to be 2
     end
 
-    context 'when the AST contains complex branching' do
-      it 'returns the number of branching points' do
-        code = <<-CODE
-        if (a && b) || c
-          do_a_thing
-        end
-        CODE
-        ast = McCabe::Parser.parse code
-        expect(McCabe::Analyzer.complexity ast).to be 3
+    it 'counts complex branches' do
+      code = <<-CODE
+      if (a && b) || c
+        do_a_thing
       end
+      CODE
+      ast = McCabe::Parser.parse code
+      expect(McCabe::Analyzer.complexity ast).to be 3
     end
 
     context 'when an Enumerable method is used' do
-      context 'with a block' do
-        it 'returns 1' do
-          code = '[1, 2].each { |e| puts e }'
-          ast = McCabe::Parser.parse code
-          expect(McCabe::Analyzer.complexity ast).to be 1
-        end
+      it 'counts a block as 1' do
+        code = '[1, 2].each { |e| puts e }'
+        ast = McCabe::Parser.parse code
+        expect(McCabe::Analyzer.complexity ast).to be 1
       end
 
-      context "with 'symbol#to_proc'" do
-        it 'returns 1' do
-          code = '[1, 2].map(&:to_s)'
-          ast = McCabe::Parser.parse code
-          expect(McCabe::Analyzer.complexity ast).to be 1
-        end
+      it 'counts a symbol#to_proc as 1' do
+        code = '[1, 2].map(&:to_s)'
+        ast = McCabe::Parser.parse code
+        expect(McCabe::Analyzer.complexity ast).to be 1
       end
 
-      context 'without a block' do
+      context 'without a block or symbol#to_proc' do
         it 'returns 0' do
           code = '[1, 2].map.each.count'
           ast = McCabe::Parser.parse code
